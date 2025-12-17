d9a0cbbd41f28f375afbf14126e6ab671feb5482
Tyler Guo <tyleguo2004@gmail.com>
2025-12-16 22:40:18 -0800

    Update stg_measurements_hours.sql

--- openaq_dbt/models/staging/stg_measurements_hours.sql
+++ openaq_dbt/models/staging/stg_measurements_hours.sql
@@ -1,16 +1,19 @@
-{{ config(
-    materialized = 'view'
-) }}
+{{ config(materialized='view') }}
 
 select
-    sensor_id,
-    location_id,
-    country,
-    latitude,
-    longitude,
-    parameter_id,
-    parameter_name,
-    units,
-    datetime_utc,
-    value
-from {{ source('openaq_raw', 'measurements_hours') }}
+  sensor_id,
+  location_id,
+  country,
+  latitude,
+  longitude,
+  parameter_id,
+  parameter_name,
+  units,
+  datetime_utc,
+  value
+from {{ source('openaq_raw', 'measurements_hours_p') }}
+qualify
+  row_number() over (
+    partition by sensor_id, datetime_utc
+    order by datetime_utc desc
+  ) = 1
